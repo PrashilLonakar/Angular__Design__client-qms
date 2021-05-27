e1b8510703b9dc59fc84dd0af40ccf64a4b56a6e
Prashil Lonakar <prashlons@gmail.com>
2021-05-27 16:09:18 +0530

    third commit

--- client-Qms/src/app/core/auth/index.ts
+++
@@ -1,5 +0,0 @@
-import { LoginComponent } from "./login/login.component";
-import { RegistrationComponent } from "./registration/registration.component";
-
-
-export const components = [LoginComponent, RegistrationComponent];
